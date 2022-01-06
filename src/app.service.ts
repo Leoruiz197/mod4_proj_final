45ea695359d14748142e23ba7e68caf3352b1d88
leoruiz197 <ruizruizleo@hotmail.com>
2022-01-05 21:49:10 -0300

    correcao

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 const lista = ['banana', 'maça', 'melão'];
 
-const listaObj = [{ nome: 'leo', email: 'leo@leo.com.br', idade: 24 }];
+const listaObj = [
+  { nome: 'leo', email: 'leo@leo.com.br', idade: 24 },
+  { nome: 'Dudinha', email: 'du@da.com.br', idade: 15 },
+  { nome: 'marcio', email: 'marcio@chamma.com.br', idade: 35 },
+];
 
 @Injectable()
 export class AppService {
