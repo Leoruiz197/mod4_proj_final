9a8da9d688c189319fa3771fb162cad3070a8e8e
leoruiz197 <ruizruizleo@hotmail.com>
2022-01-05 22:31:53 -0300

    final

--- FRONT/script.js
+++ FRONT/script.js
@@ -1,7 +1,19 @@
-fetch("https://mod4-proj-final.herokuapp.com/lista")
+const ul = document.getElementById('lista');
+
+fetch("http://localhost:3000/listaobj")
 .then((resp) => resp.json())
   .then(function(data) {
-    console.log("Resposta da API", data)
-  }).catch(function(error) {
+    const filmes = data;
+    console.log(data);
+    return filmes.map(function(filme) {
+      const li = document.createElement("li");
+
+      const span = document.createElement('span');
+      span.innerHTML = `${filme.nome} - ${filme.email} - ${filme.idade}`;
+      
+      li.appendChild(span);
+      ul.appendChild(li);
+    })
+}).catch(function(error) {
   console.log(error);
 });
