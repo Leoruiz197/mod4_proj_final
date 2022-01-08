a644bd66e59bc72e6d9d1cca22431d21e9fcf7e9
leoruiz197 <ruizruizleo@hotmail.com>
2022-01-07 21:15:07 -0300

    swagger

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: 'Paulo Salvatore',
+    description: `O nome será utilizado para qualquer coisa (Perfil, Home Page, etc) que precise exibir informações da pessoa conectada.`,
+  })
+  nome: string; 
+}
